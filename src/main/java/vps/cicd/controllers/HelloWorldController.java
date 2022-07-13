3f996bf711ed6b49c4cab149040e57e02860f163
l3043y <boreyc2013@gmail.com>
2022-07-13 16:40:00 +0700

    add main java

---
+++ src/main/java/vps/cicd/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package vps.cicd.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
